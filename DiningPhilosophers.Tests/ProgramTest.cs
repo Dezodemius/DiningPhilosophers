9bcc0eaa71565904c9077a1560cd4f8d898c10d6
Егор <gladkovyegor@gmail.com>
2021-03-14 17:48:42 +0400

    Добавил тестов

--- DiningPhilosophers.Tests/ProgramTest.cs
+++ DiningPhilosophers.Tests/ProgramTest.cs
@@ -6,20 +6,47 @@ namespace DiningPhilosophers.Tests
 {
   public class Tests
   {
-    [Test]
-    public void ProgramContinuousTest()
+    [TestCase(1)]
+    [TestCase(5)]
+    [TestCase(10)]
+    public void ProgramContinuousTestForMinutes(int minute)
     {
       var programTask = Task.Factory.StartNew(Program.Main);
-      var stopwatch = Task.Factory.StartNew(RunStopwatch);
-      
+      var stopwatch = Task.Factory.StartNew(
+        () => RunStopwatch(DateTime.Now.AddMinutes(minute)));
       Task.WaitAll(programTask, stopwatch);
       
       Assert.Pass();
     }
 
-    private static void RunStopwatch()
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(4)]
+    [TestCase(8)]
+    public void ProgramContinuousTestForHours(int hour)
     {
-      var finishDatetime = DateTime.Now.AddDays(1);
+      var programTask = Task.Factory.StartNew(Program.Main);
+      var stopwatch = Task.Factory.StartNew(
+        () => RunStopwatch(DateTime.Now.AddMinutes(hour)));
+      Task.WaitAll(programTask, stopwatch);
+      
+      Assert.Pass();
+    }
+    
+    [TestCase(1)]
+    [TestCase(2)]
+    public void ProgramContinuousTestForDays(int days)
+    {
+      var programTask = Task.Factory.StartNew(Program.Main);
+      var stopwatch = Task.Factory.StartNew(
+        () => RunStopwatch(DateTime.Now.AddMinutes(days)));
+      Task.WaitAll(programTask, stopwatch);
+      
+      Assert.Pass();
+    }
+    
+    private static void RunStopwatch(DateTime finishDatetime)
+    {
       while (finishDatetime > DateTime.Now) { }
     }
   }
